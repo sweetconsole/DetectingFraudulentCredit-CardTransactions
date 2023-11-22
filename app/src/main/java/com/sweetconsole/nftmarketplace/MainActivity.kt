c5d7c05650f3f155deee27e4a8dad092bc8b9f9d
Artem4ik8bit <77201676+Artem4ik8bit@users.noreply.github.com>
2023-11-22 12:28:49 +0300

    Update app

--- app/src/main/java/com/sweetconsole/nftmarketplace/MainActivity.kt
+++ app/src/main/java/com/sweetconsole/nftmarketplace/MainActivity.kt
@@ -8,15 +8,14 @@ import com.google.firebase.storage.storage
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
-
-    private val reference = Firebase.storage.reference.child("van-gogh/van-gogh-1.jpg")
+    
     private val url = "https://gas-kvas.com/uploads/posts/2023-02/1675344318_gas-kvas-com-p-van-gog-art-risunok-37.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageView: ImageView = findViewById(R.id.nftImage)
+        val imageView: ImageView = findViewById(R.id.nftImage)  
 
         Picasso.get()
             .load(url)
